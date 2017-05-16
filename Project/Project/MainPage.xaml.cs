cb6ac751fb07704d76df09bd9ab5bc73b8f169c1
Divine Mutanga <sanjas154@gmail.com>
2017-05-15 20:51:24 -0500

    Final submission

--- Project/Project/MainPage.xaml.cs
+++ Project/Project/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -14,38 +16,75 @@ namespace Project
         public MainPage()
         {
             InitializeComponent();
-          
+            
 
 
-        }
-        private void OnAcceptButtonClicked(object sender, EventArgs e)
-        {
-            //  string text = SchoolEntry.Text;
-            //SchoolLabel.Text = "School" + text;
 
         }
+        String SUBMIT = "";
+        String DECLINE = "";
+        String RESET = "";
+        
+       
+
+
         private void OnSubmitButtonClicked(object sender, EventArgs e)
         {
-            //  string text = SchoolEntry.Text;
-            //SchoolLabel.Text = "School" + text;
-
+            
+            Button button = (Button)sender;
+            SUBMIT = button.Text;
+            
         }
         private void OnDeclineButtonClicked(object sender, EventArgs e)
         {
-            //  string text = SchoolEntry.Text;
-            //SchoolLabel.Text = "School" + text;
+            Button button = (Button)sender;
+            DECLINE = button.Text;
 
         }
         private void OnResetButtonClicked(object sender, EventArgs e)
         {
-            //  string text = SchoolEntry.Text;
-            //SchoolLabel.Text = "School" + text;
+            School.Text = "";
+            Location.Text = ""; 
+            FName.Text = "";
+            LName.Text = "";
+            NName.Text = "";
+            Entry.Text = "";
+            Exit.Text = "";
+            Major.Items.Clear();
+             
+            Button button = (Button)sender;
+            RESET = button.Text;
 
+        }
+        private void OnAcceptButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            {
+                object entry = null;
+                if (entry == null)
+                    return;
+                Picker picker = (Picker)sender;
+                int selectedIndex = picker.SelectedIndex;
+                if (selectedIndex == -1)
+                    return;
+                string selectedItem = picker.Items[selectedIndex];
+                //PropertyInfo propertyInfo = typeof(Keyboard).GetRuntimeProperty(selectedItem);
+                //Collection views;
+                //entry.Keyboard = (Keyboard)propertyInfo.GetValue(null);
+                
+            }
+           
         }
 
 
 
+    }
 
+    internal class Collection
+    {
     }
 }
         
